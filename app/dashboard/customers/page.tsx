454e38ad1dee0c0adf9a1d8e418f6e723db17f7d
KevinU20221275 <U20221275@univo.edu.sv>
2025-01-26 13:22:41 -0600

    Finish the course nextjs-dashboard

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata : Metadata = {
+    title: 'Customers',
+    description: 'Customers Overview',
+}
+
 export default function Page(){
     return <p>Customers Page</p>
 }
